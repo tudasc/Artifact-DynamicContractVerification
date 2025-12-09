e244694d459b05239833ca2f9f4ac24780b8ef9f
N00byKing <N00byKing@hotmail.de>
2025-12-09 10:39:52 +0100

    Fix broken tests from upstream

--- Correctness_Sources/RMARaceBench/MIXED/remote/004-MPI-shmem-mixed-get-get-remote-no.cpp
+++ Correctness_Sources/RMARaceBench/MIXED/remote/004-MPI-shmem-mixed-get-get-remote-no.cpp
@@ -19,8 +19,8 @@ int main(int argc, char** argv)
 
     // Initialise MPI
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
     // Initialise SHMEM
     shmem_init();
